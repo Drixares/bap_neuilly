493f5f91514415276924317819227185c39e8a39
Mattéo <matteo.lorenzoq@gmail.com>
2025-03-17 10:07:33 +0100

    feat: added admin dashboard template

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                // A retirer plus tard
+                hostname: "res.cloudinary.com",
+                protocol: "https",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
