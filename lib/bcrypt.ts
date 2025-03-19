6bc5bbed9685464e7d91766696ed555176b3ae64
Mattéo <matteo.lorenzoq@gmail.com>
2025-03-19 14:51:26 +0100

    feat: added bcryptjs utilities and setup safe-action

---
+++ lib/bcrypt.ts
@@ -0,0 +1,10 @@
+import { compare, hash } from "bcryptjs";
+
+export async function hashPassword(password: string) {
+    return await hash(password, 10);
+}
+
+export async function comparePassword(password: string, hashedPassword: string) {
+    return await compare(password, hashedPassword);
+}
+
