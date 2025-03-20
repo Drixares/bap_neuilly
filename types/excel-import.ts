51e93c95eb02426320aae14f3c0a19e3c1adf6af
Paul Boisaubert-Baillion <Pl.Baillion@gmail.com>
2025-03-20 10:35:37 +0100

    fix: resolve conflicts

--- types/excel-import.ts
+++ types/excel-import.ts
@@ -1,5 +1,6 @@
-import { ProcessedUserSchema } from "@/app/schema";
+import { ProcessedUserSchema, ProcessedBusinessSchema } from "@/app/schema";
 import { z } from "zod";
 
 export type ProcessedUserData = z.infer<typeof ProcessedUserSchema>;
+export type ProcessedBusinessData= z.infer<typeof ProcessedBusinessSchema>;
 export type ExcelRow = Record<string, unknown>;
