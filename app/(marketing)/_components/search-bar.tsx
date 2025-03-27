9ad09f8e95b480d569ad9a46189673df5cea9fef
Paul Boisaubert-Baillion <Pl.Baillion@gmail.com>
2025-03-27 18:00:31 +0100

    feat: finished creator-list page

--- app/(marketing)/_components/search-bar.tsx
+++ app/(marketing)/_components/search-bar.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
-export default function SearchBar() {
+interface SearchBarProps {
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+    placeholder?: string;
+}
+
+export default function SearchBar({ value, onChange, placeholder = "Rechercher un exposant..." }: SearchBarProps) {
     return (
         <div className="flex flex-row p-2 bg-[var(--beige)] border-none rounded-xl mb-2">
-            <input type="text" placeholder="Rechercher" className="p-1 bg-[var(--beige)] text-[#552D08] border-none"/>
+            <input type="text" placeholder={placeholder} value={value} onChange={onChange} className="p-1 bg-[var(--beige)] text-[#552D08] border-none"/>
             <Image src='/icon/search-icon.svg' width={25} height={25} alt='search icon' />
         </div>
     );
